8e3a22a44636e2c5972811dc270acc32fb30667f
Andrew Breckenridge <asbreckenridge@me.com>
2015-01-05 18:48:29 -0800

    add transitions, configure the login buttons

--- All Hands/WelcomeViewController.swift
+++ All Hands/WelcomeViewController.swift
@@ -21,15 +21,17 @@ class WelcomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginWithFacebookHit(sender: AnyObject) {
+        
     }
-    */
-
+    
+    @IBAction func loginWithEmailHit(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func signupWithEmailHit(sender: AnyObject) {
+        
+    }
+    
+    
 }
